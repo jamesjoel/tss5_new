66685bca54e4a5ee8a6317d97728225d3974f209
jamesjoel <james.joelj@gmail.com>
2019-09-03 19:03:03 +0530

    find and insert in mongodb

--- pro6/app.js
+++ pro6/app.js
@@ -1,6 +1,38 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017";
+/*
+MongoClient.connect(url, function(err, result){
+    if(err){
+        console.log("Connection Error", err);
+        
+    }
+    var db = result.db("tss5"); // use tss5
+    // db.student.find()
+    db.collection("student").find().toArray(function(err, result){
+        if(err){
+            console.log("Collection Error", err);
+        }
+        console.log(result);
+    });
+
+
+});*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.use(express.static(__dirname+"/public"));
@@ -12,22 +44,39 @@ app.use(bodyParser());
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("index");
+
+    MongoClient.connect(url, function(err, result){
+        var db = result.db("tss5");
+        db.collection("student").find().toArray(function(err, result){
+
+            var obj = { result : result };
+            res.render("index", obj);
+
+        });
+    });
+
+
+
+
+
+
+
 });
+app.get("/add", function(req, res){
+    res.render("add");
+});
+
+
+
 
 app.post("/save", function(req, res){
-
-    var a = req.body.f_name;
-    var b = req.body.email;
-    var c = req.body.add;
-    console.log(req.body);
-    // save data into DB
-    res.redirect("/about");
-
-});
-
-app.get("/about", function(req, res){
-    res.render("about");
+    
+    MongoClient.connect(url, function(err, result){
+        var db = result.db("tss5");
+        db.collection("student").insert(req.body, function(err, result){
+            res.redirect("/");
+        });
+    });
 });
 
 
