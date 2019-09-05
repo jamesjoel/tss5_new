ff4591daec53677fef05d50d08847e8523636fad
jamesjoel <james.joelj@gmail.com>
2019-09-05 14:27:35 +0530

    ejs and ejs loop

---
+++ akhilesh/pro2/app.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var app = express();
+
+
+app.set("view engine", "ejs");
+
+
+
+
+app.use(express.static(__dirname+"/public"));
+
+
+
+app.get("/", function(req, res){
+    var a = "Rohit";
+    var b = 25;
+
+    var obj={ name : a, age : b, city : "indore" };
+    
+    res.render("home", obj);
+});
+
+app.get("/list", function(req, res){
+    var data = [
+        {
+            name : "rohit",
+            age : 25,
+            city : "indore"
+        },
+        {
+            name: "james",
+            age: 25,
+            city: "indore"
+        },
+        {
+            name: "jaya",
+            age: 25,
+            city: "ujjain"
+        },
+        {
+            name: "nidhi",
+            age: 25,
+            city: "bhopal"
+        }
+    ];
+
+    var obj = { stu : data };
+
+    res.render("list", obj);
+});
+
+
+
+ 
+app.listen(3000, function(){
+    console.log("Server Running");
+});
