d2e2dd53db8e23217b608c2fdf4c3c3c6eb3e3b6
jamesjoel <james.joelj@gmail.com>
2019-09-16 18:58:40 +0530

    CRUD Complete

--- crud/app.js
+++ crud/app.js
@@ -13,6 +13,9 @@ var MongoClient = require("mongodb").MongoClient;
 // make Mongodb URL for connection
 var dbUrl = "mongodb://localhost:27017";
 
+
+var mongo = require("mongodb");
+
 // setting a view engine
 app.set("view engine", "ejs");
 
@@ -29,6 +32,7 @@ app.get("/", function(req, res){
     var pagedata = { title : "Home Page", pagename : "home"};
     res.render("layout", pagedata);
 });
+
 app.get("/student", function (req, res) {
     MongoClient.connect(dbUrl, function(err, client){
         var db = client.db("tss5");
@@ -40,6 +44,72 @@ app.get("/student", function (req, res) {
         });
     });
 });
+
+app.get("/add", function (req, res) {
+    MongoClient.connect(dbUrl, function(err, client){
+        var db = client.db("tss5");
+        db.collection("city").find().toArray(function(err, result){
+            var pagedata = { title: "Student Add", pagename: "add", result : result};
+            res.render("layout", pagedata);
+
+        });
+    });
+
+
+
+
+    
+});
+
+
+app.post("/add", function(req, res){
+
+    MongoClient.connect(dbUrl, function(err, client){
+        var db = client.db('tss5');
+        db.collection("student").insert(req.body, function(err, result){
+            res.redirect("/student");
+        });
+    });
+});
+
+app.get("/delete/:id", function(req, res){
+    console.log(req.params);
+    MongoClient.connect(dbUrl, function(err, client){
+        var db = client.db("tss5");
+        db.collection("student").remove({ _id : mongo.ObjectId(req.params.id)}, function(err, result){
+            res.redirect("/student");
+        });
+    });
+});
+
+app.get("/edit/:id", function(req, res){
+    MongoClient.connect(dbUrl, function (err, client) {
+        var db = client.db("tss5");
+        db.collection("city").find().toArray(function (err, result1) {
+
+            db.collection("student").find({ _id : mongo.ObjectId(req.params.id)}).toArray(function(err, result2){
+                var pagedata = { title: "Student Edit", pagename: "edit", result: result1, user:result2[0] };
+                res.render("layout", pagedata);
+
+            });
+        });
+    });
+});
+
+app.post("/edit", function(req, res){
+    console.log(req.body);
+    MongoClient.connect(dbUrl, function(err, client){
+        var db = client.db("tss5");
+        db.collection("student").update({ _id : mongo.ObjectId(req.body.sid)}, { $set : req.body }, function(err, result){
+            res.redirect("/student");
+        });
+    });
+
+
+});
+
+
+
 
 
 
