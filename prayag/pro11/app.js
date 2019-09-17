4a236ffa97f5eb536a77a02768dd160760506a27
Rishu99077 <rishu99077@gmail.com>
2019-09-17 15:36:25 +0530

    update pro11

--- prayag/pro11/app.js
+++ prayag/pro11/app.js
@@ -4,6 +4,9 @@ var bodyParser=require("body-parser");
 var MongoClient=require("mongodb").MongoClient;
 var Dburl="mongodb://localhost:27017";
 
+
+var mongo=require("mongodb");
+
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser());
@@ -16,7 +19,7 @@ app.get("/",function(req,res){
 });
 
 
-app.get("/add",function(req,res) {
+app.get("/add",function(req,res){
     MongoClient.connect(Dburl,function(err,client){
         var db=client.db("tss5");
         db.collection("city").find().toArray(function(err,result){
@@ -26,7 +29,49 @@ app.get("/add",function(req,res) {
 
         });
     });
+
 });
+
+
+
+app.post("/add",function(req,res){
+    console.log(req.body)
+    MongoClient.connect(Dburl,function(err,client){
+        var db=client.db("tss5")
+        db.collection("student").insert(req.body,function(err,result){
+            res.redirect("/student")
+        })
+    })
+})
+
+
+app.get("/student",function(req,res){
+    MongoClient.connect(Dburl,function(err,client){
+        var db=client.db("tss5")
+        db.collection("student").find().toArray(function(err,result){
+     var pagedata={title:"Student Page",pagename:"student",result:result}
+     res.render("layout",pagedata);
+            
+        })
+    })
+
+})
+
+app.get("/delete/:id",function(req,res){
+    console.log(req.params);
+    MongoClient.connect(Dburl,function(err,client){
+        var db=client.db("tss5");
+        db.collection("student").remove({_id:mongo.ObjectId(req.params.id)},function(err,result){
+            res.redirect("/student");
+        })
+    })
+})
+
+
+
+
+
+
 
 var port=process.env.PORT || 3000;
 
