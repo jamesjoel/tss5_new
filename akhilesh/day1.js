e23eafbb74d9a89490025f2e7bc3d6657bbb4d7e
hardikjoshi04@gmail <hardikjoshi04@gmail.com>
2019-09-18 18:43:49 +0530

    second change akhilesh sir

---
+++ akhilesh/day1.js
@@ -0,0 +1,31 @@
+//  Data Types in Javascript
+// 1. Initiger (number)
+// 2. String
+// 3. Float (number)
+// 4. Boolean
+// 5. Array (collection of multipal values witch can any datatypes)
+// 6. Object
+
+
+var a = "rohit"; // string
+var b = 100; // int (number)
+var d = 54.21; // float (number)
+var c = true; // boolean (true/false)
+
+var arr = [];
+
+var data = ["red", "green", "blue", 100, 504.22, true];
+
+
+//  Object -- Right now is a variable data-type
+var obj = {};
+var obj = { name : "Rohit", age : 25, city : "indore", abc :"bhopal", xyz : "" };
+
+
+obj.country = "India";
+console.log(obj);
+
+
+
+
+
