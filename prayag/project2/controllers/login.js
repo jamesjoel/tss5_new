f8d6933075c80d6c34ae1fed4b24250c49c7166d
prayag <prayagpatidar786@gmail.com>
2019-09-24 12:56:13 +0530

    project2 login page

--- prayag/project2/controllers/login.js
+++ prayag/project2/controllers/login.js
@@ -1,6 +1,7 @@
 var express=require("express")
 var routes=express.Router()
-
+var User=require("../models/user")
+var sha1=require("sha1")
 
 
 routes.get("/",function(req,res){
@@ -8,6 +9,26 @@ routes.get("/",function(req,res){
     res.render("layout",pagedata)
 })
 
+routes.post("/",function(req,res){
+    var e=req.body.email
+    var p=req.body.password
+    User.find({email:e},function(err,result){
+        console.log(req.body)
+        console.log(result)
+        if(result.length==0){
+            console.log("this email and password is incorrect.")
+        }
+        else{
+            if(result[0].password==sha1(p)){
+                console.log("correct")
+                res.redirect("/user")
+            }
+            else{
+                console.log("password is incorrect.")
+            }
+        }
+    })
+})
 
 
 
