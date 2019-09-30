b8f1c992c5b8090e1ab5db6a06c3a64ada9b7dfa
tss9 <prayagpatidar786@gmail.com>
2019-09-30 19:14:08 +0530

    active and deactive user

--- prayag/project3/controllers/signup.js
+++ prayag/project3/controllers/signup.js
@@ -13,6 +13,7 @@ routes.get("/",function(req,res){
 routes.post("/",function(req,res){
 
     req.body.password=sha1(req.body.password)
+    req.body.status=1
     console.log(req.body)
 
     User.insert(req.body,function(err,result){
