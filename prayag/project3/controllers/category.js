e1359e3f4019b36f611f52c4a42ff48bedcd3062
tss9 <prayagpatidar786@gmail.com>
2019-10-03 17:50:09 +0530

    category on front

--- prayag/project3/controllers/category.js
+++ prayag/project3/controllers/category.js
@@ -7,11 +7,11 @@ var Product=require("../models/product")
 routes.get("/:name",function(req,res){
     // console.log(req.params)
     Category.find({name:req.params.name},function(err,result1){
-        // console.log(result1)
+        console.log(result1)
         var _id=result1[0]._id
         _id=String(_id)
         Product.find({category:_id},function(err,result){
-            console.log(result)
+            // console.log(result)
             var pagedata={title:"Category:"+ req.params.name,pagename:"home/index",result:result}
             res.render("layout",pagedata)
         })
