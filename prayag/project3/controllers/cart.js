34f401933471100f63102ec31f8a65af8d61d079
prayag <prayagpatidar786@gmail.com>
2019-10-04 16:18:03 +0530

    ejs of cart

--- prayag/project3/controllers/cart.js
+++ prayag/project3/controllers/cart.js
@@ -1,10 +1,42 @@
 var express=require("express")
 var routes=express.Router()
+var mongo=require("mongodb")
+var Product=require("../models/product")
 
 
 routes.get("/",function(req,res){
+    var ids=req.cookies.cartItem
+    var arr=ids.split("#")
+    var where_arr=[]
+    arr.forEach(function(x){
+        var obj={_id :mongo.ObjectId(x)}
+        where_arr.push(obj)
+    })
 
+    Product.find({$or:where_arr},function(err,result){
+        console.log(result)
+        var pagedata={title:"My Cart",pagename:"cart/index",result:result}
+        res.render("layout",pagedata)
+
+    })
 })
+
+routes.get("/delete_item/:id",function(req,res){
+    console.log(req.params)
+    var where={_id:mongo.ObjectId(req.params)}
+    Product.delete(where,function(err,result){
+        res.redirect("/cart")
+    })
+})
+
+routes.get("/clearcart",function(req,res){
+    res.clearCookie("cartItem")
+    res.redirect("/")
+})
+
+
+
+
 
 routes.get("/add/:id",function(req,res){
     // console.log(req.params)
