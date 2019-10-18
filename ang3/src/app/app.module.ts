4eed90069cb71185a2c908df45b187106c8809b9
jamesjoel <james.joelj@gmail.com>
2019-10-18 19:25:22 +0530

    angular day3

--- ang3/src/app/app.module.ts
+++ ang3/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BoxComponent } from './box/box.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
   declarations: [
+
     AppComponent,
-    BoxComponent
+
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- ang3/src/app/home/home.component.spec.ts
+++ ang3/src/app/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BoxComponent } from './box.component';
+import { HomeComponent } from './home.component';
 
-describe('BoxComponent', () => {
-  let component: BoxComponent;
-  let fixture: ComponentFixture<BoxComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BoxComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BoxComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
