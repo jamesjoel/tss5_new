2e1eb58fa63ea059ff323ea97913995cdefd7de9
prayag <prayagpatidar786@gmail.com>
2019-10-21 14:17:21 +0530

    different user different event not done

--- prayag/task/controllers/user.js
+++ prayag/task/controllers/user.js
@@ -2,16 +2,26 @@ var express=require("express")
 var routes=express.Router()
 var Event=require("../models/event")
 var User=require("../models/user")
+var mongo=require("mongodb")
 
 
 
 routes.get("/",function(req,res){
-    Event.find({},function(err,result){
-        var pagedata={title:"Profile Page",pagename:"user/index",result:result}
-        res.render("layout",pagedata)
+
+    var where=mongo.ObjectId(req.session.uid)
+    User.find({_id:where},function(err,result1){
+        console.log(result1)
+        Event.find({},function(err,result){
+            console.log(result)
+            var pagedata={title:"Profile Page",pagename:"user/index",result:result,result1:result1}
+            res.render("layout",pagedata)
+
+        })
 
     })
+
 })
+
 
 routes.get("/event",function(req,res){
     var n=req.query.n
@@ -25,6 +35,7 @@ routes.get("/event",function(req,res){
 })
 routes.post("/event",function(req,res){
     // console.log(req.body)
+    req.body.uid=mongo.ObjectId(req.body.uid)
     Event.insert(req.body,function(err,result){
         res.redirect("/user")
     })
