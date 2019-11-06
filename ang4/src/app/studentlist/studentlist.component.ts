01978074db72ac6529918adc7a0979919beb6113
jamesjoel <james.joelj@gmail.com>
2019-11-06 19:13:11 +0530

    CRUD in angular

--- ang4/src/app/studentlist/studentlist.component.ts
+++ ang4/src/app/studentlist/studentlist.component.ts
@@ -1,24 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+interface student{
+  id? : number,
+  name : string,
+  age : number,
+  fee : number
+  // address? : string
+  // color[] : any
+}
+
+
 @Component({
   selector: 'app-studentlist',
   templateUrl: './studentlist.component.html',
   styleUrls: ['./studentlist.component.scss']
 })
 export class StudentlistComponent implements OnInit {
-  studentArr = [
+
+  askStuObj:student={
+    name : "",
+    age : null,
+    fee : null
+  };
+
+  studentArr:student[] = [
     {
+      id : 1,
       name : "rohit",
       age : 25,
       fee : 5000
     },
     {
+      id : 2,
       name:"nidhi",
       age : 21,
       fee : 6500
+    },
+    {
+      id : 3,
+      name : "james",
+      age : 23,
+      fee : 6800
+    },
+    {
+      id : 4,
+      name : "jaya",
+      age : 30,
+      fee : 4900
     }
   ];
-  newStudent={
+  newStudent:student={
     name : "",
     age : null,
     fee : null
@@ -33,7 +64,47 @@ export class StudentlistComponent implements OnInit {
 
   save(){
     // console.log(this.newStudent);
-    this.studentArr.push(this.newStudent);
+    if(this.newStudent.id)
+    {
+      for(var i=0; i<this.studentArr.length; i++)
+      {
+        if(this.studentArr[i].id==this.newStudent.id)
+        {
+          this.studentArr[i]=this.newStudent;
+        }
+      }
+    }
+    else{
+
+      this.newStudent.id = this.studentArr.length+1;
+      this.studentArr.push(this.newStudent);
+      this.newStudent = {
+        name: "",
+        age: null,
+        fee: null
+      }
+    }
+
+  }
+  askAdd(){
+    /*
+    this.newStudent = {
+      name: "",
+      age: null,
+      fee: null
+    }
+    */
+  }
+  askDelete(a:student){
+    this.askStuObj=a; 
+  }
+  delete(){
+    var n = this.studentArr.indexOf(this.askStuObj);
+    this.studentArr.splice(n, 1);
+  }
+  askEdit(a:student){
+    // this.newStudent=a;
+    this.newStudent = Object.assign({}, a);
   }
 
 }
