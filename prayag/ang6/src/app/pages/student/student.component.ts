dff6f6e79392147b13f373981a31c989a4c9131d
prayag <prayagpatidar786@gmail.com>
2019-11-19 16:53:00 +0530

    connection with nodejs and mongodb data fetch

---
+++ prayag/ang6/src/app/pages/student/student.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../../services/student.service';
+import { Student } from '../../models/student.interface';
+
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.scss']
+})
+export class StudentComponent implements OnInit {
+
+  allStudent:Student[];
+
+  constructor(private _student:StudentService) { }
+
+  ngOnInit() {
+    this._student.getStudent()
+                 .subscribe(data => this.allStudent=data); 
+  }
+
+  comeStudent(student:Student){
+    this.allStudent.push(student);
+  }
+
+}
+ 
