1dde8b84518b539e3c12c1d3ad5e6ac164a6a27e
jamesjoel <james.joelj@gmail.com>
2019-11-19 11:48:43 +0530

    new project ang6 for AJAX

---
+++ ang6/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentComponent } from './pages/student/student.component';
+import { AddstudentComponent } from './sharedcomponent/addstudent/addstudent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    StudentComponent,
+    AddstudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
