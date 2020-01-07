1608d73f2f35498f9060753ebb2839f7a8536534
Harshal <harshalchoudhary007@gmail.com>
2020-01-07 18:51:16 +0530

    pages making and design part

--- prayag/makaan/makaan/src/app/app-routing.module.ts
+++ prayag/makaan/makaan/src/app/app-routing.module.ts
@@ -1,8 +1,50 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BuyComponent } from './pages/buy/buy.component';
+import { SellComponent } from './pages/sell/sell.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"about",
+    component:AboutComponent
+  },
+  {
+    path:"buy",
+    component:BuyComponent
+  },
+  {
+    path:"sell",
+    component:SellComponent
+  },
+  {
+    path:"contact",
+    component:ContactComponent
+  },
+  {
+    path:"register",
+    component:RegisterComponent
+  },
+  {
+    path:"*",
+    component:PagenotfoundComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- prayag/makaan/makaan/src/app/app.module.ts
+++ prayag/makaan/makaan/src/app/app.module.ts
@@ -7,10 +7,30 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BuyComponent } from './pages/buy/buy.component';
+import { SellComponent } from './pages/sell/sell.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { FooterComponent } from './sharedcomponent/footer/footer.component';
+import { JumbotronComponent } from './sharedcomponent/jumbotron/jumbotron.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    BuyComponent,
+    SellComponent,
+    ContactComponent,
+    AboutComponent,
+    PagenotfoundComponent,
+    FooterComponent,
+    JumbotronComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
